e98c1fccef58ad3f341b6ebb5d5d31968f114a7d
Ben Watt <ben.watt@slicedbread.co.uk>
2017-06-18 21:24:31 +0100

    Created help module for api routes

---
+++ benandkatiegetmarried/benandkatiegetmarried/Content/HelpApi.js
@@ -0,0 +1,21 @@
+﻿/// reference path="https://code.jquery.com/jquery-3.2.1.min.js"
+
+(function helpApi($) {
+
+    $('document').ready((ev) => {
+        $(".method:contains('GET')").css('color', 'green');
+        $(".method:contains('POST')").css('color', 'black');
+        $(".method:contains('PUT')").css('color', 'orange');
+        $(".method:contains('DELETE')").css('color', 'red');
+    });
+
+    $('.modules').click((ev) => {
+
+        var moduleId = ev.delegateTarget.id.toString().replace('.', '\\.');
+        var routes = $('#' + moduleId).children('.routes');
+
+        routes.toggle();
+
+    });
+
+})($);
