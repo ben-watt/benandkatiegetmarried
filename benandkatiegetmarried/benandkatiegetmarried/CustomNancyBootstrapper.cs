2a85b6a404a2d79b2b09cd756f08ab0b0c814f3e
Ben Watt <ben.watt@slicedbread.co.uk>
2017-08-17 23:00:08 +0100

    Accept api header for CORS request

--- benandkatiegetmarried/benandkatiegetmarried/CustomNancyBootstrapper.cs
+++ benandkatiegetmarried/benandkatiegetmarried/CustomNancyBootstrapper.cs
@@ -81,7 +81,7 @@ namespace benandkatiegetmarried
             {
                 ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
                             .WithHeader("Access-Control-Allow-Methods", "POST,GET")
-                            .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+                            .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type, X-Requested-With");
             });
 
             var modules = this.GetAllModules(context);
