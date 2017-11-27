2f2d54a542c8b2a2e65cafa0d06034c1871c1d15
Ben Watt <ben.watt@slicedbread.co.uk>
2017-11-27 18:21:30 +0000

    Updated guest csv

---
+++ benandkatiegetmarried/PostgreMigrations/Scripts/Script0005CreateResponsesView.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [core].[Responses] AS
+SELECT g.InviteId,
+g.FirstName,
+g.LastName,
+rr.Response,
+rr.MealChoice,
+r.SongLink,
+r.DietaryRequirements
+FROM core.RsvpResponses AS rr
+    INNER JOIN core.Rsvps AS r
+	   ON r.Id = rr.RsvpId
+    INNER JOIN core.Guests AS g
+	   ON g.Id = rr.GuestId
